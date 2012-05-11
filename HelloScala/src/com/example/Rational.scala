30fb377f7b0faa209a79fb7fe2794a52da34e153
Łukasz Siwiński <lsiwinski@gmail.com>
2012-05-12 01:20:30 +0200

    added require function & override toString function in Rational class

--- HelloScala/src/com/example/Rational.scala
+++ HelloScala/src/com/example/Rational.scala
@@ -1,3 +1,6 @@
 package com.example
 
-class Rational(val nom:Int, val denom:Int)
+class Rational(val nom:Int, val denom:Int) {
+  require(denom!=0)
+  override def toString(): String = (nom+"/"+denom)
+}
--- HelloScala/src/com/example/RationalTest.scala
+++ HelloScala/src/com/example/RationalTest.scala
@@ -3,7 +3,14 @@ package com.example
 object RationalTest {
 
   def main(args: Array[String]): Unit = {
+    
     val half=new Rational(1,2)
+    println(half.nom+"/"+half.denom)
+    
+    println("and now i'll try use toString method");
+    println(half)
+    
+    val dontDooooThat=new Rational(1,0)
     println(half.nom+"/"+half.denom)
   }
   
