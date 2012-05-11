8535bda31fa15be8486d27ef79a90049b4446aa0
Łukasz Siwiński <lsiwinski@gmail.com>
2012-05-12 01:12:10 +0200

    Rational class

---
+++ HelloScala/src/com/example/Rational.scala
@@ -0,0 +1,3 @@
+package com.example
+
+class Rational(val nom:Int, val denom:Int)
---
+++ HelloScala/src/com/example/RationalTest.scala
@@ -0,0 +1,10 @@
+package com.example
+
+object RationalTest {
+
+  def main(args: Array[String]): Unit = {
+    val half=new Rational(1,2)
+    println(half.nom+"/"+half.denom)
+  }
+  
+}
