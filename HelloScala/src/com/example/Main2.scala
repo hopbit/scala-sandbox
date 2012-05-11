98f2a77e59fb3fd069af3cc57d79e34f02f22474
Łukasz Siwiński <lsiwinski@gmail.com>
2012-05-12 01:00:06 +0200

    kurs ze scala.net.pl - pierwsze kroki w scali

---
+++ HelloScala/src/com/example/Main2.scala
@@ -0,0 +1,12 @@
+package com.example
+
+object Main2 {
+	def main(args: Array[String]): Unit = {
+	  println(2+2);
+	  // wypisuje argumenty na konsoli
+	  args.foreach(println(_));
+	  // w javie wyglada to mniej wiecej tak:
+	  // for(....
+	  
+	}
+}
