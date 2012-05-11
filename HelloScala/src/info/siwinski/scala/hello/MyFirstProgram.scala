a6cf1350e3c38ece0a5e50987bafd85a219aba40
Łukasz Siwiński <lsiwinski@gmail.com>
2012-05-12 00:46:38 +0200

    Hello Scala project; my not so first code in scala

---
+++ HelloScala/src/info/siwinski/scala/hello/MyFirstProgram.scala
@@ -0,0 +1,9 @@
+package info.siwinski.scala.hello
+
+object MyFirstProgram {
+
+  def main(args: Array[String]): Unit = {
+    println("Hopbit just started writing in Scala, but I'm not sure if it rocks :P");
+  }
+
+}
